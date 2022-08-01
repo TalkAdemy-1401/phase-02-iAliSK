9fc7796c4599b4d25260ad24a2c1f1c89799dae4
iAliSK <aebrahimi6739@gmail.com>
2022-08-01 14:39:50 +0430

    Revert "write employee list to xml file"
    This reverts commit 7ff0a0283a722cb9befd4934aaaa3f8e9744e5ee.

--- Phase02/JsonToXml/src/main/java/Main.java
+++ Phase02/JsonToXml/src/main/java/Main.java
@@ -4,15 +4,10 @@ import java.util.List;
 public class Main {
 
     private static final String jsonPath = "../../EmployeeData.json";
-    private static final String xmlPath = "../../EmployeeData.xml";
 
     public static void main(String[] args) throws IOException {
         // read employee list from json file
         JsonManager jsonManager = new JsonManager();
         List<Employee> employeeList = jsonManager.readList(jsonPath);
-
-        // write employee list to xml file
-        XmlManager xmlManager = new XmlManager();
-        xmlManager.writeList(employeeList, xmlPath);
     }
 }
