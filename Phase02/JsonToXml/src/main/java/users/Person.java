781131eb99c441f868cca262f65f498dd800ebb0
alisk <aliebrahimi6739@gmail.com>
2022-08-03 12:05:15 +0430

    add parent class 'Person' to 'Employee'

---
+++ Phase02/JsonToXml/src/main/java/users/Person.java
@@ -0,0 +1,67 @@
+package users;
+
+public class Person {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String about;
+    private String country;
+    private int gender;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAbout() {
+        return about;
+    }
+
+    public void setAbout(String about) {
+        this.about = about;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+}
